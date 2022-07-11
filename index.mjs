880d17c565d7c2f79fd3f871eb388e0d56da5638
neidz <adamo7255@gmail.com>
2022-07-11 19:19:38 +0200

    fix: module extension and relay starting high

--- index.mjs
+++ index.mjs
@@ -6,6 +6,7 @@ const port = 8080;
 
 // relay signal pin is connected to GPIO4 on raspberry pi, it's set as output because it's only used for sending signal, it's not used for reading
 const relay = new Gpio(4, 'out');
+relay.writeSync(0);
 
 app.get('/', (req, res)=> {
     res.status(200).send('there will be page on that route soon but for now go to /open to you know, open');
